3246ac6fe6e4312d1ce96034d0aa91dd249308b7
Ayomiidey <quadriayomiidey@gmail.com>
2024-10-08 14:24:55 +0100

    error fix

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
-export class AboutComponent {}
+export class AboutComponent {
+  isExpanded: boolean = false;
+
+  toggleText() {
+    this.isExpanded = !this.isExpanded;
+  }
+}
