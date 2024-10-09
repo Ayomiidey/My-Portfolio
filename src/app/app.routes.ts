d886063c41afd4a428393c94c2253270fde704fd
Ayomiidey <quadriayomiidey@gmail.com>
2024-10-09 16:41:09 +0100

    project page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ServicesComponent } from './services/services.component';
 
 export const routes: Routes = [
   { path: '', component: LayoutComponent },
@@ -11,4 +12,5 @@ export const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactsComponent },
   { path: 'project', component: ProjectsComponent },
+  { path: 'services', component: ServicesComponent },
 ];
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from '../home/home.component';
 import { HeaderComponent } from '../header/header.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HomeComponent, HeaderComponent],
+  imports: [HomeComponent, HeaderComponent, AboutComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
