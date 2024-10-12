1aa74747d65aeef69e966c948739148d549d42b4
Ayomiidey <quadriayomiidey@gmail.com>
2024-10-12 15:06:33 +0100

    update

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-about',
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 import { HomeComponent } from '../home/home.component';
 import { HeaderComponent } from '../header/header.component';
 import { AboutComponent } from '../about/about.component';
+import { ServicesComponent } from '../services/services.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { ContactsComponent } from '../contacts/contacts.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HomeComponent, HeaderComponent, AboutComponent],
+  imports: [
+    HomeComponent,
+    HeaderComponent,
+    AboutComponent,
+    ServicesComponent,
+    ProjectsComponent,
+    ContactsComponent,
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
