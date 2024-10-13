d95ceaabf6ca5a14df88792a8a1e0302527f35e8
Ayomiidey <quadriayomiidey@gmail.com>
2024-10-13 15:32:25 +0100

    service page update

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,10 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css',
 })
-export class ServicesComponent {}
+export class ServicesComponent {
+  selectedService: string | null = null;
+
+  toggleServiceDescription(service: string) {
+    this.selectedService = this.selectedService === service ? null : service;
+  }
+}
