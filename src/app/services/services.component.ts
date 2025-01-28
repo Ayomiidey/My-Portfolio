e8a6731e881f9450b9d0be9c789f81c70a82f2d6
Ayomiidey <quadriayomiidey@gmail.com>
2025-01-28 17:24:37 +0100

    update

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-services',
@@ -10,6 +10,42 @@ import { Component } from '@angular/core';
 })
 export class ServicesComponent {
   selectedService: string | null = null;
+
+  // @ViewChild('webSection') webSection!: ElementRef;
+  // @ViewChild('uiuxSection') uiuxSection!: ElementRef;
+  // @ViewChild('apiSection') apiSection!: ElementRef;
+
+  // toggleServiceDescription(service: string) {
+  //   if (this.selectedService === service) {
+  //     this.selectedService = null; // Collapse the section
+  //   } else {
+  //     this.selectedService = service; // Expand the section
+  //     this.scrollToSection(service); // Scroll to the corresponding section
+  //   }
+  // }
+
+  // scrollToSection(service: string) {
+  //   let section: ElementRef;
+
+  //   switch (service) {
+  //     case 'web':
+  //       section = this.webSection;
+  //       break;
+  //     case 'uiux':
+  //       section = this.uiuxSection;
+  //       break;
+  //     case 'api':
+  //       section = this.apiSection;
+  //       break;
+  //     default:
+  //       return;
+  //   }
+
+  //   section.nativeElement.scrollIntoView({
+  //     behavior: 'smooth',
+  //     block: 'start',
+  //   });
+  // }
 
   toggleServiceDescription(service: string) {
     this.selectedService = this.selectedService === service ? null : service;
